2d507bb98c9dd5baa65c859ef94a5c45d3fda244
Hardy Perez <perezleonrobinsonh@gmail.com>
2026-07-12 21:51:59 -0400

    Configurar PostgreSQL en Render

--- build.sh
+++ build.sh
@@ -5,4 +5,27 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
-python manage.py createsuperuser --noinput || true
+
+python manage.py shell -c "
+import os
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+username = os.environ['DJANGO_SUPERUSER_USERNAME']
+email = os.environ['DJANGO_SUPERUSER_EMAIL']
+password = os.environ['DJANGO_SUPERUSER_PASSWORD']
+
+user, created = User.objects.get_or_create(
+    username=username,
+    defaults={'email': email}
+)
+
+user.email = email
+user.is_staff = True
+user.is_superuser = True
+user.is_active = True
+user.set_password(password)
+user.save()
+
+print('Administrador creado o actualizado correctamente')
+"
